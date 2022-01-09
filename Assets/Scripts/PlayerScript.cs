3de520050eaa94a91d158ead875c8a7a4d64ffd0
gsproston <gsproston94@gmail.com>
2022-01-09 22:55:59 +0000

    Start zone

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,14 @@ public class PlayerScript : MonoBehaviour
     float horizontal;
     float vertical;
 
+    bool isInStartZone;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        isInStartZone = true;
     }
 
     // Update is called once per frame
@@ -25,8 +30,33 @@ public class PlayerScript : MonoBehaviour
     {
         // do not read input here as FixedUpdate is not always running and inputs can be missed
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        if (isInStartZone)
+        {
+            horizontal = Math.Max(Math.Abs(horizontal), Math.Abs(vertical));
+            position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        }
+        else
+        {
+            position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+            position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        }
         rigidbody2d.MovePosition(position);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "StartZone")
+        {
+            isInStartZone = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.tag == "StartZone")
+        {
+            isInStartZone = false;
+        }
     }
 }
