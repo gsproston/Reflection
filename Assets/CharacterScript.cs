e9d608db9746ae540f4ede9ed0d40f3861726bf5
gsproston <gsproston94@gmail.com>
2022-02-14 21:51:04 +0000

    Made a character which moves after a few seconds

---
+++ Assets/CharacterScript.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    float speed = 4.0f;
+    DateTime startTime = DateTime.UtcNow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        DateTime currentTime = DateTime.UtcNow;
+        if (currentTime.CompareTo(startTime.AddSeconds(5)) > 0)
+        {
+            Vector2 position = transform.position;
+            position.x = position.x + speed * Time.deltaTime;
+            transform.position = position;
+        }
+    }
+}
